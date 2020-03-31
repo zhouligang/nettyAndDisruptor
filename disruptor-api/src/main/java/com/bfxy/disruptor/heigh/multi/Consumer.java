7affd08683ccc2de77e58fb8de36396e6806e3b4
周黎钢 <zhouligang2006@163.com>
2020-03-31 17:58:24 +0800

    first commit
    Change-Id: I4a5ff978b598b2e9839e83855daf2ff40dda8f0e

---
+++ disruptor-api/src/main/java/com/bfxy/disruptor/heigh/multi/Consumer.java
@@ -0,0 +1,29 @@
+package com.bfxy.disruptor.heigh.multi;
+
+import com.lmax.disruptor.WorkHandler;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Consumer implements WorkHandler<Order> {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+    private String comsumerId;
+    private Random random = new Random();
+
+    public Consumer(String comsumerId) {
+        this.comsumerId = comsumerId;
+    }
+
+    public void onEvent(Order event) throws Exception {
+        Thread.sleep(1 * random.nextInt(5));
+        System.err.println("当前消费者: " + this.comsumerId + ", 消费信息ID: " + event.getId());
+        count.incrementAndGet();
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+
+
+}
